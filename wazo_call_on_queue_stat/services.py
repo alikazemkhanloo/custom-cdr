d85e53be7e9f980deec3a3cd1acbe71c727b003a
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-02-05 17:31:37 +0330

    update queue service

--- wazo_call_on_queue_stat/services.py
+++ wazo_call_on_queue_stat/services.py
@@ -56,7 +56,6 @@ class StatCallOnQueueService:
         items=[]
         if interval and from_ and until:
             for start, end in _generate_interval(interval, from_= from_, until=until, timezone=timezone):
-                print('start,and', start, end)
                 result = self.dao.get_call_on_queue_stat_by_agent(
                     agent_id=agent_id,
                     tenant_uuid=tenant_uuid,
@@ -76,9 +75,38 @@ class StatCallOnQueueService:
                 items.append(output)
         return items
 
-    def get_stat_for_queue(self, queue_id, tenant_uuid, **args):
-        result = self.dao.get_call_on_queue_stat_by_queue(
-            queue_id=queue_id, tenant_uuid=tenant_uuid, **args
-        )
-        output = [row._asdict() for row in result]
-        return output
+    def get_stat_for_queue(
+        self,
+        queue_id,
+        tenant_uuid,
+        week_days=None,
+        start_time=None,
+        end_time=None,
+        from_=None,
+        until=None,
+        timezone=None,
+        interval=None,
+    ):
+        timezone = pytz.timezone(timezone)
+        items=[]
+        if interval and from_ and until:
+            for start, end in _generate_interval(interval, from_= from_, until=until, timezone=timezone):
+                result = self.dao.get_call_on_queue_stat_by_queue(
+                    queue_id=queue_id,
+                    tenant_uuid=tenant_uuid,
+                    week_days=week_days,
+                    start_time=start_time,
+                    end_time=end_time,
+                    from_=start,
+                    until=end,
+                    timezone=timezone,
+                )
+                output = {
+                    'start': start.isoformat(),
+                    'end': end.isoformat(),
+                    'interval': interval,
+                    'data': [row._asdict() for row in result]
+                }
+                items.append(output)
+        return items
+
