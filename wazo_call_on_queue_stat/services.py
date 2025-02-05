8fd2864892e28d717fc11bdd10177f181756371f
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-02-05 13:59:43 +0330

    .

--- wazo_call_on_queue_stat/services.py
+++ wazo_call_on_queue_stat/services.py
@@ -1,14 +1,25 @@
 from wazo_call_on_queue_stat.schemas import CallOnStatQueueResultSchema
 from . import dao
 
-class StatCallOnQueueService():
+
+class StatCallOnQueueService:
     def __init__(self, _dao):
         self.dao: dao = _dao
 
     def get_stat_for_agent(self, agent_id, tenant_uuid, **args):
-        result = self.dao.get_call_on_queue_stat_by_agent(agent_id=agent_id, tenant_uuid=tenant_uuid, **args)
-        return CallOnStatQueueResultSchema().dump(result)
-    
+        result = self.dao.get_call_on_queue_stat_by_agent(
+            agent_id=agent_id, tenant_uuid=tenant_uuid, **args
+        )
+        output = []
+        for row in result:
+            output.append(dict(row))
+        return output
+
     def get_stat_for_queue(self, queue_id, tenant_uuid, **args):
-        result = self.dao.get_call_on_queue_stat_by_queue(queue_id=queue_id, tenant_uuid=tenant_uuid, **args)
-        return CallOnStatQueueResultSchema().dump(result)
+        result = self.dao.get_call_on_queue_stat_by_queue(
+            queue_id=queue_id, tenant_uuid=tenant_uuid, **args
+        )
+        output = []
+        for row in result:
+            output.append(dict(row))
+        return output
