5ec4d2c7541b9df10f7a9942256c0612b5238293
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-02-05 14:37:40 +0330

    .

--- wazo_call_on_queue_stat/services.py
+++ wazo_call_on_queue_stat/services.py
@@ -10,18 +10,12 @@ class StatCallOnQueueService:
             agent_id=agent_id, tenant_uuid=tenant_uuid, **args
         )
         print(result)
-        output = []
-        for row in result:
-            print(type(row))
-            print(dir(row))
-            output.append(row._as_dict())
+        output = [row._asdict() for row in result]
         return output
 
     def get_stat_for_queue(self, queue_id, tenant_uuid, **args):
         result = self.dao.get_call_on_queue_stat_by_queue(
             queue_id=queue_id, tenant_uuid=tenant_uuid, **args
         )
-        output = []
-        for row in result:
-            output.append(row._as_dict())
+        output = [row._asdict() for row in result]
         return output
